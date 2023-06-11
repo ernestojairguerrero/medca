6d84e9dba6c9cb1980e1e961bdb933b30115351f
josefandinod <josefandinod@gmail.com>
2023-06-10 21:19:52 -0500

    listar banks, pendientes images

---
+++ src/app/pages/bancos/banks.models.ts
@@ -0,0 +1,8 @@
+
+export interface listBanksModel {
+  id: number;
+  name: string;
+  slog: string;
+  created_at: string;
+  updated_at: string;
+}
--- src/app/pages/bancos/banks.service.ts
+++ src/app/pages/bancos/banks.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, tap } from 'rxjs';
+import { GetServerError } from 'src/app/helpers/get-server-error';
+import { environment } from 'src/environments/environment';
+import { listBanksModel } from './banks.models';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +12,42 @@ export class BanksService {
 
 
   private uploadUrl = 'http://127.0.0.1:8000/api/banco-create';
+  private _apiUrl = environment.apiUrl;
+
+  private banks: BehaviorSubject<any> = new BehaviorSubject(null);
+  private errorServer: BehaviorSubject<any> = new BehaviorSubject(null);
 
   private _http = inject(HttpClient);
+  private _getServerError = inject(GetServerError);
 
+  get banks$(): Observable<any> {
+    return this.banks.asObservable();
+  }
   
   uploadFile(file: File, name: string): Observable<any> {
     const formData = new FormData();
     formData.append('slog', file);
     formData.append('name', name);
 
-    return this._http.post(this.uploadUrl, formData);
+    return this._http.post(this._apiUrl + 'banco-create', formData);
+  }
+
+  listBanks(): Observable<listBanksModel[]> {
+    return this._http.get(`${this._apiUrl}banco`,)
+      .pipe(
+        tap((documents: any) => this.banks.next(documents)),
+        catchError(error => {
+          let errorMsg: string;
+          if (error.error instanceof ErrorEvent) {
+            errorMsg = `Error: ${error.error.message}`;
+          } else {
+            errorMsg = this._getServerError.extractError(error);
+          }
+          this._getServerError.showError(errorMsg);
+          this.errorServer.next(errorMsg);
+          return error;
+        })
+      );
   }
 
 }
